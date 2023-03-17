e5d2d0a1d0632b6476065b540fe705729ac5c58d
Ujjwal Chowdhury <90385123+U77w41@users.noreply.github.com>
2023-03-17 17:18:42 +0530

    Add files via upload

---
+++ nthBday/__init__.py
@@ -0,0 +1,14 @@
+
+"""__init__"""
+
+
+from .nthBday import *
+
+__version__ = '0.2.7'
+__maintainer__ = 'Ujjwal Chowdhury'
+
+__all__ = ['first_working_day','second_working_day','third_working_day','fourth_working_day','fifth_working_day','nth_working_day',
+           'last_working_day','nth_last_working_day','last_sun','last_sat','last_fri','last_thu','last_wed',
+           'last_tue','last_mon','second_sat','fourth_sat']
+
+
